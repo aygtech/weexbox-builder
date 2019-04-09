74e07f1e06f7b714f218ffd147c8825d0d07a811
Mario <myeveryheart@qq.com>
2019-04-09 10:28:15 +0800

    和debugger打通

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const WeexBuilder = require('./lib/webpack/weexBuilder');
-exports.build = function (source, dest, options, debugConfig, callback) {
-  if (!source || typeof source !== 'string' || (Array.isArray(source) && source.length === 0)) {
-    return Promise.reject('The source path can not be empty!');
-  }
-  if (!dest || typeof source !== 'string') {
-    return Promise.reject('The output path can not be empty or non-string!');
-  }
-  return new WeexBuilder(source, dest, options, debugConfig).build(callback);
+const { WeexBuilder } = require("./lib/webpack/WeexBuilder");
+
+const build = async (source, target, options, callback) => {
+  return new Promise((resolve, reject) => {
+    if (
+      !source ||
+      typeof source !== "string" ||
+      (Array.isArray(source) && source.length === 0)
+    ) {
+      reject(new Error("The source path can not be empty!"));
+    }
+    if (!target || typeof source !== "string") {
+      reject(new Error("The output path can not be empty or non-string!"));
+    }
+    resolve(new WeexBuilder(source, target, options).build(callback));
+  });
 };
+
+module.exports = build
